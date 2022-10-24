baba2c11b1b5e591e7ae91d856608903cf8cdd86
Assem92 <as.aoussar@gmail.com>
2022-10-24 15:40:25 +0200

    Ajout de la question 1

--- Exercice1/Princ.java
+++ Exercice1/Princ.java
@@ -4,6 +4,7 @@ public class Princ {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Question1 : La différence entrre Somme et Sommable est que Sommable contient un argument de + 
 		    Somme<Integer> sommeEntier = (a, b) -> a + b;
 	        Somme<Double> sommeDouble = (a, b) -> a + b;
 	        Somme<Long> sommeLong = (a, b) -> a + b;
